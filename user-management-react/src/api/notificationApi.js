aa664a6460b9f70eba52e6ecc6093a607b0ba9a0
SaintsaminSalem <salemekaria@gmail.com>
2026-08-03 21:18:12 +0100

    done

--- user-management-react/src/api/notificationApi.js
+++ user-management-react/src/api/notificationApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000/api/notifications";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/notifications`;
 
 export const getMyNotifications = async () => {
   const token = localStorage.getItem("token");
