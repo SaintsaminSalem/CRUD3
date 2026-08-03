1ecf4c5554c6bea840b1c16b5c61e963e356dea4
SaintsaminSalem <salemekaria@gmail.com>
2026-08-03 21:22:56 +0100

    done

--- user-management-react/src/socket.js
+++ user-management-react/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000"; // match your backend port
+const SOCKET_URL = `${import.meta.env.VITE_API_URL}/socket`; // match your backend port
 
 let socket = null;
 
